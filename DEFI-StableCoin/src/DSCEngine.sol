baa7857dc1f777c8f4ba2a2635e8743dae746724
0x4nud33p <anudeepavula009@gmail.com>
2025-03-12 16:56:27 +0000

    update

--- DEFI-StableCoin/src/DSCEngine.sol
+++ DEFI-StableCoin/src/DSCEngine.sol
@@ -33,7 +33,6 @@ import { DecentralizedStableCoin } from "./DecentralizedStableCoin.sol";
 
 /*
  * @title DSCEngine
- * @author Patrick Collins
  *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg at all times.
  * This is a stablecoin with the properties:
